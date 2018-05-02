3602cd8f0ef01829f769b132f9916ba3398e5bb4
gaoyang <2313332818@qq.com>
2018-05-02 14:50:43 +0800

    add zh_CN ' 确定删除备份口令？ '

--- src/js/services/confirmDialog.js
+++ src/js/services/confirmDialog.js
@@ -1,34 +1,32 @@
-
 'use strict';
 
-angular.module('copayApp.services').factory('confirmDialog', function($log, $timeout, gettextCatalog, isCordova) {
-  var root = {};
+angular.module('copayApp.services').factory('confirmDialog', function ($log, $timeout, gettextCatalog, isCordova) {
+	var root = {};
+	var acceptMsg = gettextCatalog.getString('Accept');
+	var cancelMsg = gettextCatalog.getString('Cancel');
+	var confirmMsg = gettextCatalog.getString('Confirm');
 
+	root.show = function (msg, cb) {
+		if (isCordova) {
+			navigator.notification.confirm(
+				msg,
+				function (buttonIndex) {
+					if (buttonIndex == 1) {
+						$timeout(function () {
+							return cb(true);
+						}, 1);
+					} else {
+						return cb(false);
+					}
+				},
+				confirmMsg, [acceptMsg, cancelMsg]
+			);
+		}
 
-  var acceptMsg = gettextCatalog.getString('Accept');
-  var cancelMsg = gettextCatalog.getString('Cancel');
-  var confirmMsg = gettextCatalog.getString('Confirm');
+		else {
+			return cb(confirm(msg));
+		}
+	};
 
-  root.show = function(msg, cb) {
-    if (isCordova) {
-      navigator.notification.confirm(
-        msg,
-        function(buttonIndex) {
-          if (buttonIndex == 1) {
-            $timeout(function() {
-              return cb(true);
-            }, 1);
-          } else {
-            return cb(false);
-          }
-        },
-        confirmMsg, [acceptMsg, cancelMsg]
-      );
-    } else {
-      return cb(confirm(msg));
-    }
-  };
-
-  return root;
+	return root;
 });
-
