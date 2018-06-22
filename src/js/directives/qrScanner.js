67f0cfee45902d3eedfa791e943ac79e299f69b2
kake <gusuncheng@gmail.com>
2018-06-22 15:01:28 +0800

    modi privilege description

--- src/js/directives/qrScanner.js
+++ src/js/directives/qrScanner.js
@@ -29,7 +29,7 @@ angular.module('copayApp.directives').directive('qrScanner', ['$rootScope', '$ti
 							window.ignoreMobilePause = false;
 							window.plugins.spinnerDialog.hide();
 						}, 100);
-						alert('Open camera permissions');
+						alert(gettextCatalog.getString('Please allow TrustNote to access your camera'));
 					}
 				);
 				if ($scope.beforeScan) {
