107f371fcfbc0f2c7df878373000082c603fafbf
kakegu <gusuncheng@gmail.com>
2018-09-17 19:35:33 +0800

    modi: change trustME URI

--- src/js/controllers/preferencesHub.js
+++ src/js/controllers/preferencesHub.js
@@ -1,50 +1,36 @@
 'use strict';
 
 angular.module('trustnoteApp.controllers').controller('preferencesHubController',
-	function($scope, $timeout, configService, go){
-		var config = configService.getSync();
-		var initHubEdit = false;
-		this.hub = config.hub;
-		this.arrHub = configService.hub;
+    function ($scope, $timeout, $log, configService, go) {
+        var config = configService.getSync();
+        this.hub = config.hub;
+        this.arrHub = configService.hub;
 
-		this.save = function() {
-			var self = this;
-			var device = require('trustnote-common/device.js');
-			var lightWallet = require('trustnote-common/light_wallet.js');
-			self.hub = self.hub.replace(/^wss?:\/\//i, '').replace(/^https?:\/\//i, '');
+        this.save = function () {
+            var self = this;
+            var device = require('trustnote-common/device.js');
+            var lightWallet = require('trustnote-common/light_wallet.js');
+            self.hub = self.hub.replace(/^wss?:\/\//i, '').replace(/^https?:\/\//i, '');
 
-			var opts = {hub: self.hub};
+            $log.debug("hub:", self.hub);
 
-			configService.set(opts, function(err) {
-				if (err) {
-					$scope.$emit('Local/DeviceError', err);
-					return;
-				}
+            var opts = { hub: self.hub };
 
-				$timeout(function () {
-					device.setDeviceHub(self.hub);
-					lightWallet.setLightVendorHost(self.hub);
-				}, 800);
+            configService.set(opts, function (err) {
+                if (err) {
+                    $log.error("save hub:", err);
+                    $scope.$emit('Local/DeviceError', err);
+                    return;
+                }
 
-				$timeout(function(){
-					go.path('preferencesGlobal');
-				}, 50);
-			});
-		};
+                $timeout(function () {
+                    device.setDeviceHub(self.hub);
+                    lightWallet.setLightVendorHost(self.hub);
+                }, 800);
 
-		var unwatchEditHub = $scope.$watch(angular.bind(this, function(){
-			return this.hub;
-		}), function(){
-			if (initHubEdit) {
-
-			}
-			else {
-				initHubEdit = true;
-			}
-		});
-
-
-		$scope.$on('$destroy', function(){
-			unwatchEditHub();
-		});
-	});
+                $timeout(function () {
+                    go.path('preferencesGlobal');
+                }, 50);
+            });
+        };
+    });
